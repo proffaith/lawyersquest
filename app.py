5854fe29ed215ba08ddfea98e8f691672bba9a2e
Tim Faith <tfaith@mmars.com>
2025-03-21 20:20:36 -0400

    damn your eyes MySQL authentication

--- app.py
+++ app.py
@@ -86,7 +86,8 @@ def get_db_connection():
         socket.socket = socks.socksocket  # Monkey patch
 
         DB_HOST=os.getenv("DB_HOST")
-        DB_USER="lawyersquest_dbadmin"
+        #DB_USER="lawyersquest_dbadmin"
+        DB_USER=os.getenv("DB_USER")
         DB_PASSWORD=os.getenv("DB_PASSWORD")
         DB_NAME=os.getenv("DB_NAME")
 
